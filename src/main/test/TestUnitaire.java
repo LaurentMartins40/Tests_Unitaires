051714e6bb9c3e0d07e45ef5fe1eeddf69b39a09
laurent <laurentmartins@outlook.com>
2019-02-22 14:17:28 +0100

    testUnitaire

---
+++ src/main/test/TestUnitaire.java
@@ -0,0 +1,14 @@
+package main.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dev.utils.StringUtils;
+
+public class TestUnitaire {
+	@Test
+	public void testLevenshteinDistance(){
+		assertTrue(StringUtils.levenshteinDistance("coucou","coucou1")==1);
+	}
+}
